c1d9614301f4efa4ebd476a45e93ce29db473d23
김주영 <98079486+Leusin@users.noreply.github.com>
2025-01-14 16:36:38 +0900

    Add renderer class

--- Week0/main.cpp
+++ Week0/main.cpp
@@ -1,5 +1,8 @@
+#pragma once
 
 #include <windows.h>
+#include "URenderer.h"
+#include "FVertexSimple.h"
 
 // 각종 메시지를 처리할 함수
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -39,6 +42,13 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, 
 	bool bIsExit = false;
 
 	// 각종 생성하는 코드를 여기에 추가합니다.
+	// Renderer Class를 생성합니다.
+	URenderer	renderer;
+
+	// D3D11 생성하는 함수를 호출합니다.
+	renderer.Create(hWnd);
+	// 렌더러 생성 직후에 쉐이더를 생성하는 함수를 호출합니다.
+	renderer.CreateShader();
 
 	// Main Loop (Quit Message가 들어오기 전까지 아래 Loop를 무한히 실행하게 됨)
 	while (bIsExit == false)
@@ -60,16 +70,53 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, 
 				break;
 			}
 		}
-
 		////////////////////////////////////////////
 		// 매번 실행되는 코드를 여기에 추가합니다.
 
+		// 준비 작업
+		renderer.Prepare();
+		renderer.PrepareShader();
 
+		// Renderer와 Shader 생성 이후에 버텍스 버퍼를 생성합니다.
+		FVertexSimple triangle_vertices[] =
+		{
+			{  0.0f,  1.0f, 0.0f,  1.0f, 0.0f, 0.0f, 1.0f }, // Top vertex (red)
+			{  1.0f, -1.0f, 0.0f,  0.0f, 1.0f, 0.0f, 1.0f }, // Bottom-right vertex (green)
+			{ -1.0f, -1.0f, 0.0f,  0.0f, 0.0f, 1.0f, 1.0f }  // Bottom-left vertex (blue)
+		};
+
+		FVertexSimple* vertices = triangle_vertices;
+		UINT ByteWidth = sizeof(triangle_vertices);
+		UINT numVertices = sizeof(triangle_vertices) / sizeof(FVertexSimple);
+
+		// 생성
+		D3D11_BUFFER_DESC vertexbufferdesc = {};
+		vertexbufferdesc.ByteWidth = ByteWidth;
+		vertexbufferdesc.Usage = D3D11_USAGE_IMMUTABLE;
+		vertexbufferdesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+
+		D3D11_SUBRESOURCE_DATA vertexbufferSRD = { vertices };
+
+		ID3D11Buffer* vertexBuffer;
+
+		renderer.Device->CreateBuffer(&vertexbufferdesc, &vertexbufferSRD, &vertexBuffer);
+
+		// 생성한 버텍스 버퍼를 넘겨 실질적인 렌더링 요청
+		renderer.RenderPrimitive(vertexBuffer, numVertices);
+
+		// 다 그렸으면 버퍼를 교환
+		renderer.SwapBuffer();
 
 		////////////////////////////////////////////
+
+		// 버텍스 버퍼 소멸은 Renderer 소멸전에 처리합니다.
+		vertexBuffer->Release();
 	}
 
-	// 소멸하는 코드를 여기에 추가합니다.
+	// 렌더러 소멸 직전에 쉐이더를 소멸 시키는 함수를 호출합니다.
+	renderer.ReleaseShader();
+	// D3D11 소멸 시키는 함수를 호출합니다.
+	renderer.Release();
 
 	return 0;
 }
